4ad1c05923efc1db7383e2c0960674c6d98c4e33
Marcin <deser1@o2.pl>
2021-05-17 23:28:52 +0200

    Add this file to project in update code

--- Book/Book.py
+++ Book/Book.py
@@ -1,12 +1,41 @@
-from tkinter.ttk import Frame, Style
-from tkinter import BOTH, Tk
+import string
+from tkinter.ttk import *
+from tkinter import *
 import sys
+
 try:
     import scribus
 except ImportError:
     print("Unable to import the 'scribus' module. This script will only run within")
     print("the Python interpreter embedded in Scribus. Try Script->Execute Script.")
     sys.exit(1)
+if not scribus.haveDoc():
+    scribus.messageBox('Scribus - Script Error', "No document open", scribus.ICON_WARNING, scribus.BUTTON_OK)
+    sys.exit(1)
+
+
+def page_count():
+    pagenum: int = scribus.pageCount()
+    return pagenum
+
+
+def reverse(text):
+    result = ""
+    for i in range(len(text), 0, -1):
+        result += text[i - 1]
+    return result
+
+
+def book_page(pages=None):
+    pagenum: int = page_count()
+    if pages is None:
+        pages = []
+    # pagenumrvs = list(reversed(pagenum))
+    for i in range(0, pagenum):
+        pages[i] = i
+        strpages: string
+        strpages += pages[i] + ','
+    return strpages
 
 
 class Okno(Frame):
@@ -25,6 +54,19 @@ class Okno(Frame):
 def main():
     gui = Tk()
     gui.geometry("1000x700")
+    # Label Page Count
+    var = StringVar()
+    var.set("Page count: " + str(page_count()))
+    label = Label(gui, textvariable=var, relief=FLAT)  # RAISED OR FLAT(DEFAULT)
+    label.pack()
+    # End Label Page Count
+    # Label Page Count
+    var2 = StringVar()
+    str_book = book_page()
+    var2.set("Page book: " + str(book_page()))
+    label2 = Label(gui, textvariable=var2, relief=FLAT)  # RAISED OR FLAT(DEFAULT)
+    label2.pack()
+    # End Label Page Count
     app = Okno(gui)
     gui.mainloop()
 
